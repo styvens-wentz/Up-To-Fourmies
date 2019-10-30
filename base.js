94dcb843064cbf9e13d35330163fdc878e0c175f
styvens59 <56022040+styvens59@users.noreply.github.com>
2019-10-30 23:47:21 +0100

    Finalisation du header, footer et bouton retour haut de page pour toute les pages.

--- base.js
+++ base.js
@@ -6,3 +6,20 @@ function hamburgerMenu() {
         hM.className = "topnav";
     }
 }
+
+jQuery(document).ready(function() {
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 200) {
+            jQuery('.cRetour').fadeIn(duration);
+        } else {
+            jQuery('.cRetour').fadeOut(duration);
+        }
+    });
+
+    jQuery('.cRetour').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
